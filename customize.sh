6d199c313565c525e79039e38a051f3d61ebec0e
dawnnnnnn <m1050596704@gmail.com>
2023-04-26 15:04:23 +0800

    增加配置备份和恢复功能

--- customize.sh
+++ customize.sh
@@ -2,14 +2,22 @@
 MODDIR=${0%/*}
 ui_print " -------------------------- "
 ui_print " ------ 安装中，请稍等 ------ "
-ui_print "
-通过开启/禁用附属模块`Alist-Encrypt开关[Switch]`来控制加密组件的开关"
+
+OLD_CONF=/data/local/tmp/Alist-Encrypt-conf/config.json
+if [ -e $OLD_CONF ];then
+ui_print "检测到本地已存在一份备份配置文件，默认恢复"
+cp -R /data/local/tmp/Alist-Encrypt-conf/ /data/adb/modules/Alist-Encrypt/dist/conf/
+ui_print "备份恢复成功"
+fi
+
+ui_print "通过开启/禁用附属模块`Alist-Encrypt开关[Switch]`来控制加密组件的开关"
 
 ui_print "
-请使用浏览器访问 http://127.0.0.1:5344/public/index.html 
+请使用浏览器访问 
+http://127.0.0.1:5344/public/index.html 
 或
 http://127.0.0.1:5344/index
-
 默认账户 admin 密码 123456"
+
 ui_print " ----- 安装已完成，请重启 ---- "
 ui_print " -------------------------- "
