6d68b6e2783c70c556be4ebf50ae3ffee46ba2db
dawnnnnnn <m1050596704@gmail.com>
2023-04-26 16:58:41 +0800

    check sdcard_rw before cp

--- service.sh
+++ service.sh
@@ -1,9 +1,29 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-while [ "$(getprop sys.boot_completed)" != "1" ]; do
-        sleep 5
-    done
+System_Activation()
+{
+	local wait_start=0
+	until [[ $(getprop sys.boot_completed) -eq 1 ]]; do
+		local wait_start=$((${wait_start} + 1))
+		[[ ${wait_start} -ge 150 ]] && exit 1
+		sleep 2
+	done
+}
+System_Activation
+
+sdcard_rw()
+{
+	local test_file="/sdcard/Android/.ALIST_ENCRYPT_TEST_FILE"
+	touch $test_file
+	while [[ ! -f $test_file ]]; do
+		touch $test_file
+		sleep 1
+	done
+	rm $test_file
+}
+sdcard_rw
+
 
 chmod 777 $MODDIR/bin/node_alist
 cd $MODDIR/lib
