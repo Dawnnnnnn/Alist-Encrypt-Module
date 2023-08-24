ab72447b6311b73ffa156d85c960b9c9ffce4891
dawnnnnnn <m1050596704@gmail.com>
2023-08-24 17:32:17 +0800

    适配上游打包策略变更

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ python3 fix_path.py
 cd alist-encrypt/node-proxy || exit
 export NODE_OPTIONS=--openssl-legacy-provider 
 npm install
-npm run build
-mv dist/index.js dist/alist_encrypt.js
-cd ../../
-mv alist-encrypt/node-proxy/dist/ .
+mv app.js alist_encrypt.js
+cd ../..
+mv alist-encrypt/node-proxy/ ./dist
 ALIST_ENCRYPT_VERSION=$(cat alist-encrypt/node-proxy/src/config.js | grep -oP "version = '\K[0-9]+\.[0-9]+\.[0-9]+")
 echo $ALIST_ENCRYPT_VERSION
 rm -rf alist-encrypt
