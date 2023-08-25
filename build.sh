15f6e048718bd998cb73f4ce91c59ab0277ce073
dawnnnnnn <m1050596704@gmail.com>
2023-08-25 15:12:09 +0800

    适配上游打包策略变化

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ sudo apt-get install git unzip nodejs -y
 git clone https://github.com/traceless/alist-encrypt
 python3 fix_path.py
 cd alist-encrypt/node-proxy || exit
-export NODE_OPTIONS=--openssl-legacy-provider 
 npm install
-mv app.js alist_encrypt.js
-cd ../..
-mv alist-encrypt/node-proxy/ ./dist
+npm run webpack
+mv dist/index.js dist/alist_encrypt.js
+cd ../../
+mv alist-encrypt/node-proxy/dist/ .
 ALIST_ENCRYPT_VERSION=$(cat alist-encrypt/node-proxy/src/config.js | grep -oP "version = '\K[0-9]+\.[0-9]+\.[0-9]+")
 echo $ALIST_ENCRYPT_VERSION
 rm -rf alist-encrypt
